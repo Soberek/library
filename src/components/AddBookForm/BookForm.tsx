07ab03d746d6592a6e871e29f7b8bb56b697fd46
Sober <59960384+Soberek@users.noreply.github.com>
2025-08-20 21:10:32 +0200

    fix: adjust BookForm width for better responsiveness

--- src/components/AddBookForm/BookForm.tsx
+++ src/components/AddBookForm/BookForm.tsx
@@ -51,7 +51,7 @@ const BookForm: React.FC<Props> = ({
       />
 
       <form
-        className={`fixed top-1/2 left-1/2 z-20 flex w-[80%] -translate-x-1/2 -translate-y-1/2 flex-col rounded-lg bg-white p-4 shadow-lg ${isFormVisible ? "fade-in" : "fade-out"}`}
+        className={`fixed top-1/2 left-1/2 z-20 flex w-full max-w-[550px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-lg bg-white px-4 py-4 shadow-lg ${isFormVisible ? "fade-in" : "fade-out"}`}
         onSubmit={handleBookSubmit}
       >
         <div className="mb-4 flex items-center justify-between">
