d51adcf073cfec49590c9b301a47751c5f9bc4fc
Sober <59960384+Soberek@users.noreply.github.com>
2025-08-22 09:55:21 +0200

    test: add initial tests for BookForm component rendering

---
+++ src/components/AddBookForm/BookForm.test.tsx
@@ -0,0 +1,55 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import BookForm from "./BookForm";
+
+// type BookFormProps = {n
+//   handleBookSubmit: (e: React.ChangeEvent<HTMLFormElement>) => Promise<any>;
+//   handleStarClick: (index: number) => void;
+//   stars: number;
+//   handleFormVisibility: () => void;
+//   isFormVisible: boolean;
+// };
+// To run tests with Jest, use the following command in your terminal:
+// npx jest
+// or if you have it installed globally:
+// jest
+// This will execute all test files (e.g., *.test.tsx) using Jest.
+
+const mockOnSubmit = jest.fn();
+const mockHandleStarClick = jest.fn();
+const mockHandleFormVisibility = jest.fn();
+
+describe("BookForm", () => {
+  test.only("renders form fields", () => {
+    const { container } = render(
+      <BookForm
+        handleBookSubmit={mockOnSubmit}
+        handleStarClick={mockHandleStarClick}
+        stars={0}
+        handleFormVisibility={mockHandleFormVisibility}
+        isFormVisible={true}
+      />,
+    );
+
+    expect(screen.getByPlaceholderText(/Tytuł/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Autor/i)).toBeInTheDocument();
+
+    const selectRead = container.querySelector("select[name='read']");
+    expect(selectRead).toBeInTheDocument();
+
+    const selectGenre = container.querySelector("select[name='genre']");
+    expect(selectGenre).toBeInTheDocument();
+
+    expect(
+      screen.getByPlaceholderText(/Przeczytane strony/i),
+    ).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Liczba stron/i)).toBeInTheDocument();
+
+    expect(screen.getByTestId("star-rating")).toBeInTheDocument();
+    expect(screen.getByTestId("cover-input")).toBeInTheDocument();
+
+    expect(
+      screen.getByRole("button", { name: /dodaj książkę/i }),
+    ).toBeInTheDocument();
+  });
+});
