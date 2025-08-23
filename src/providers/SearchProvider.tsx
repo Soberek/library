7923625e6a667670b2f1fddbd07b1767671fd086
Sober <59960384+Soberek@users.noreply.github.com>
2025-08-23 19:48:25 +0200

    feat: add SearchProvider context for managing search term state

---
+++ src/providers/SearchProvider.tsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState } from "react";
+
+type SearchContextType = {
+  searchTerm: string;
+  setSearchTerm: (term: string) => void;
+};
+
+const SearchContext = createContext<SearchContextType | undefined>(undefined);
+
+export const SearchProvider: React.FC<React.PropsWithChildren<{}>> = ({
+  children,
+}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  console.log(searchTerm);
+
+  return (
+    <SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearch = () => {
+  return useContext(SearchContext);
+};
