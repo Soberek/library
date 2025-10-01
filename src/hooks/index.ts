cf4c68cbed8dc5294abd648c8a7398f379f98811
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-01 19:20:27 +0200

    feat: add hooks barrel export file

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+// Barrel exports for hooks
+export { useBooks } from "./useBooks";
+export { useSearch } from "./useSearch";
+export { useUser } from "./useUser";
+export { useDebounce } from "./useDebounce";
+export { useLocalStorage } from "./useLocalStorage";
