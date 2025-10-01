48d63677807f48f613d74e942e6527d79ca392d8
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-01 19:20:40 +0200

    feat: add types barrel export file

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+// Barrel exports for types
+export type { Book, BookStatus, BookFormData, BookToAdd, BookUpdateData } from "./Book";
+export type { User } from "./User";
+export type { 
+  AppError, 
+  ValidationError, 
+  FirebaseError, 
+  NetworkError, 
+  ErrorType 
+} from "./Error";
