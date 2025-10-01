8aa96b91ff9b0d58dfa0a56d190c113569313027
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-01 19:18:27 +0200

    feat: add ErrorBoundary to main App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import MobileNavbar from "./components/MobileNavbar";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <ErrorBoundary>
       <Navbar />
       <MobileNavbar />
       <Outlet />
-    </>
+    </ErrorBoundary>
   );
 }
 
