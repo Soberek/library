2dc8b464f69a08c768e3e8a0d734a3d1aff30edb
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-01 19:18:25 +0200

    feat: enhance Book types with union types and form data types

--- src/types/Book.ts
+++ src/types/Book.ts
@@ -1,12 +1,23 @@
+export type BookStatus = "W trakcie" | "Przeczytana" | "Porzucona";
+
 export interface Book {
   id: string;
   title: string;
   author: string;
-  read: string;
+  read: BookStatus;
   overallPages: number;
   readPages?: number;
-  cover: string;
+  cover?: string;
   genre: string;
   rating: number;
   createdAt?: string;
 }
+
+export type BookFormData = Omit<Book, "id" | "createdAt">;
+
+export interface BookToAdd extends Omit<Book, "id"> {
+  userId: string;
+  createdAt: string;
+}
+
+export type BookUpdateData = Partial<Omit<Book, "id" | "createdAt">>;
