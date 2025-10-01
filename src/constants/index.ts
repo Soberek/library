ec9c6c4cfc101e27bfd3b8cab9a118b525d74f25
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-01 19:20:22 +0200

    feat: add constants barrel export file

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+// Barrel exports for constants
+export { BOOK_STATUSES, BOOK_STATUS_LABELS, getNextBookStatus } from "./bookStatus";
+export { GENRES } from "./genres";
+export { VALIDATION_RULES, ERROR_MESSAGES } from "./validation";
