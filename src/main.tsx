d2ff5b6e015d6b4dfd7618a4aeaaa37c478a36c0
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-11 14:40:30 +0200

    refactor: Resolve linting issues in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { UserProvider } from "./providers/UserContext.tsx";
-import { SearchProvider } from "./providers/SearchProvider.tsx";
-import { ThemeProvider } from "./theme";
-import { FilterProvider } from "./contexts/FilterContext.tsx";
-import RouterProviderWrapper from "./providers/RouterProvider.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { UserProvider } from './providers/UserContext.tsx';
+import { SearchProvider } from './providers/SearchProvider.tsx';
+import { ThemeProvider } from './theme';
+import { FilterProvider } from './contexts/FilterContext.tsx';
+import RouterProviderWrapper from './providers/RouterProvider.tsx';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <UserProvider>
       <ThemeProvider>
