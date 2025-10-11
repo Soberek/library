f4718f1f394daedcf67e1c1aa3305bdc048a021b
Sober <59960384+Soberek@users.noreply.github.com>
2025-10-11 14:39:15 +0200

    refactor: Remove unused imports and variables in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
-import { Navbar, MobileNavbar, ErrorBoundary } from "./components";
-import "./App.css";
+import { Navbar, MobileNavbar, ErrorBoundary } from './components';
+import './App.css';
 
 function App() {
   return (
